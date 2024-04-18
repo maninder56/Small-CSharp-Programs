80ad2277be42b3df6b0f17f3322b267d744a09d8
Maninder Singh <maninder123450@gmail.com>
2024-04-18 22:42:53 +0100

    save file

--- SeaBattleGame/SeaBattleGame/Interface.cs
+++ SeaBattleGame/SeaBattleGame/Interface.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Net.Sockets;
 using static System.Console;
 
 namespace SeaBattleGame.Interface;
 
 internal class Interface
 {
-    
+    string gameName = "Sea Battle";
+    bool quit = false;
+    int score = 0;
+    string playerName = "";
+
+    public void LaunchGame()
+    {
+        
+    }
+
 }
