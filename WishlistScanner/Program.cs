145aa75aca1f7e925e37828c40f021ea2b056a4f
Maninder Singh <maninder123450@gmail.com>
2024-12-21 15:48:29 +0000

    added wishlist files to git ignore list

---
+++ WishlistScanner/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console; 
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Clear(); 
+        WriteLine(new string('-',WindowWidth)); 
+        WriteLine(); 
+
+
+    }
+}
+
+
+// Reading WishList
+// search for this string "wishlist-list-item-1-tile#title"
+internal class ReadingWishList
+{
+    
+}
