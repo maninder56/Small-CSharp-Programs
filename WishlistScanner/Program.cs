a7dcf9ff4d9afb0b1b08802f2cecc87a4f0d32c7
Maninder Singh <maninder123450@gmail.com>
2024-12-27 18:12:52 +0000

    created method with using regular expression to find game names

--- WishlistScanner/Program.cs
+++ WishlistScanner/Program.cs
@@ -15,8 +15,7 @@ internal class Program
         wishList.WriteAllGamesName(); 
 
 
-        
-
+        WriteLine(); 
     }
 }
 
@@ -26,7 +25,7 @@ internal class Program
 internal class ReadingWishList
 {
     // readonly string filePath = Path.Combine(Environment.CurrentDirectory,"WishlistFiles", "Wishlist copy.html"); 
-    readonly string filePath = Path.Combine(Environment.CurrentDirectory,"WishlistFiles", "test.html"); 
+    readonly string filePath = Path.Combine(Environment.CurrentDirectory,"WishlistFiles", "Wishlist.html"); 
     public bool doesFileExist { get; }
 
     public ReadingWishList()
@@ -52,12 +51,51 @@ internal class ReadingWishList
     {
         using (StreamReader reader = new StreamReader(filePath)) 
         {
-            string? line = reader.ReadToEnd();         
+            string? htmlFile = reader.ReadToEnd();         
 
+            // (?<=\d+-tile#title">).+?(?=<\/div>)
+            // (?<=\d+-tile#title">)(.+?)(&amp)?(.+?)(?=<\/div>)
+
+            string namePattern = @"(?<=\d+-tile#title"">)(?'GameName'.+?)(?=<\/div>)"; 
+
+            Regex regex = new Regex(namePattern); 
+
+            MatchCollection namesFound = regex.Matches(htmlFile); 
             
-            
+            int gameCount = 0; 
+
+            WriteLine("Games saved in Wishlist:\n"); 
+
+            foreach(Match match in namesFound)
+            {
+                string name = match.Groups["GameName"].Value; 
+
+                if (name.Contains("&amp;"))
+                {
+                    name = name.Remove(name.IndexOf("&amp;"), "&amp; ".Count()); 
+                }
+
+                WriteLine("{0,-2}: {1}", ++gameCount, name); 
+            }
+
+            WriteLine("\nTotal Games: {0}", gameCount); 
+
         }
     }
 
+    // (?<=\d+-tile#title">)(.+?)(?=<\/div>)(.+?)(?<=\d+-tile#price#display-price)(.+?)(?<=class="psw-m-r-3">)(.+?)(?=<\/span>)
+    // (?<=\d+-tile#title">)(.+?)(?=<\/div>)(.+?)(?<=class="psw-m-r-3">)(.+?)(?=<\/span>)
+    // (?<=<li class=)(.+?)(?=<\/li>)
+    // (?<=\d+-tile#title">)(.+?)(?=<\/div>)(.+?)(?<=class="psw-m-r-3">)(.+?)(?=<\/span)(.+?)(?=<\/span) -- sort of works
 
+    
+
+}
+
+public class GameInfo
+{
+    // write class to store game info 
+    // name, current price, discount
+    // if discounted show original price
+    
 }
