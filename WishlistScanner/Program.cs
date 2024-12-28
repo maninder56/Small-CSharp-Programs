bc866c4bae9019b434f75f20b842dcc913b38f56
Maninder Singh <maninder123450@gmail.com>
2024-12-28 09:50:09 +0000

    added method to filter games with price

--- WishlistScanner/Program.cs
+++ WishlistScanner/Program.cs
@@ -12,7 +12,7 @@ internal class Program
 
         ReadingWishList wishList = new ReadingWishList(); 
 
-        wishList.WriteAllGamesName(); 
+        // wishList.WriteAllGamesName(); 
 
 
         WriteLine(); 
@@ -56,12 +56,13 @@ internal class ReadingWishList
             // (?<=\d+-tile#title">).+?(?=<\/div>)
             // (?<=\d+-tile#title">)(.+?)(&amp)?(.+?)(?=<\/div>)
 
-            string namePattern = @"(?<=\d+-tile#title"">)(?'GameName'.+?)(?=<\/div>)"; 
+            string namePattern = 
+                @"(?<=\d+-tile#title"">)" +     // Positive lookbehind
+                @"(?'GameName'.+?) " +          // To capture game name
+                @"(?=<\/div>)";                 // Positive lookahead 
 
             Regex regex = new Regex(namePattern); 
-
             MatchCollection namesFound = regex.Matches(htmlFile); 
-            
             int gameCount = 0; 
 
             WriteLine("Games saved in Wishlist:\n"); 
@@ -69,17 +70,13 @@ internal class ReadingWishList
             foreach(Match match in namesFound)
             {
                 string name = match.Groups["GameName"].Value; 
-
                 if (name.Contains("&amp;"))
                 {
                     name = name.Remove(name.IndexOf("&amp;"), "&amp; ".Count()); 
                 }
-
                 WriteLine("{0,-2}: {1}", ++gameCount, name); 
             }
-
             WriteLine("\nTotal Games: {0}", gameCount); 
-
         }
     }
 
@@ -88,14 +85,59 @@ internal class ReadingWishList
     // (?<=<li class=)(.+?)(?=<\/li>)
     // (?<=\d+-tile#title">)(.+?)(?=<\/div>)(.+?)(?<=class="psw-m-r-3">)(.+?)(?=<\/span)(.+?)(?=<\/span) -- sort of works
 
-    
+    public List<GameInfo> GetAllGamesInfo()
+    {
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string? htmlFile = reader.ReadToEnd(); 
+            string gameInfoPattern = 
+                @"(?<=\d+-tile#title"">)" + 
+                @"(?'GameName'.+?)" +           // Group 1 : To capture game name
+                @"(?=<\/div>)" + 
+                @"(.+?)" +                      // Group 2 
+                @"(?<=class=""psw-m-r-3"">)" +
+                @"(?'GamePrice'.+?)" +          // Group 3 : To cpature current game price
+                @"(?=<\/span)" +
+                @"(?'Discount'.+?)" +           // Group 4 : To capure Discount and original price
+                @"(?=.<\/span)"; 
+
+            Regex regex = new Regex(gameInfoPattern); 
+            MatchCollection gameFound = regex.Matches(htmlFile); 
+            List<GameInfo> gameList = new List<GameInfo>(); 
+
+            foreach(Match match in gameFound)
+            {
+                gameList.Add(new GameInfo(
+                    match.Groups["GameName"].Value,
+                    match.Groups["GamePrice"].Value,
+                    match.Groups["Discount"].Value
+                )); 
+            }
+            return gameList;
+        }
+    }
 
 }
 
 public class GameInfo
 {
-    // write class to store game info 
-    // name, current price, discount
-    // if discounted show original price
-    
+    public string Name { get; set; }
+    public string? CurrentPrice { get; set; }
+    public bool Discounted { get; set; }
+    public string? OriginalPrice {get; set; }
+    public GameInfo(string name, string price, string originalPrice)
+    {
+        Name = name; 
+        CurrentPrice = price; 
+        
+        if (originalPrice.Contains("Original price"))
+        {
+            Discounted = true; 
+
+            OriginalPrice = originalPrice.Substring(
+                startIndex: originalPrice.IndexOf("Original price") + 
+                "Original price, ".Length
+            ); 
+        }
+    } 
 }
