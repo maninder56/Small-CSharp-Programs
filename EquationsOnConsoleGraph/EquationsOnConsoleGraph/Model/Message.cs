e511920bc0b69e0a2ea500e5559ad660d64e3a05
Maninder Singh <maninder123450@gmail.com>
2025-05-17 15:41:44 +0100

    Added message class for views

---
+++ EquationsOnConsoleGraph/EquationsOnConsoleGraph/Model/Message.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquationsOnConsoleGraph.Model; 
+
+public static class Message
+{
+    private static readonly ConsoleColor currentColor = Console.ForegroundColor; 
+
+    public static ConsoleColor Color { get; private set; }
+
+    public static StringBuilder Content { get; private set; } = new StringBuilder();
+
+    public static bool MessagePending { get; private set; } = false; 
+
+    public static void AddMessage(MessageType type, string message)
+    {
+        Content.Append(message);
+
+        Color = type switch
+        {
+            MessageType.Warning => ConsoleColor.Yellow,
+            MessageType.Error => ConsoleColor.Red,
+            _ => currentColor
+        }; 
+
+        MessagePending = true;
+    }
+
+    public static void ClearMessage()
+    {
+        if (MessagePending)
+        {
+            Color = currentColor;
+            Content.Clear();
+            MessagePending = false;
+        }
+    }
+}
+
+
---
+++ EquationsOnConsoleGraph/EquationsOnConsoleGraph/Model/MessageType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquationsOnConsoleGraph.Model;
+
+public enum MessageType
+{
+    Informaiton, Warning, Error
+};
